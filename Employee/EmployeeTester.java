a9ddaac01e424399323fbedaff4de77ecfa161e7
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-02-22 23:03:18 -0600

    Update Employee

--- Employee/EmployeeTester.java
+++ Employee/EmployeeTester.java
@@ -5,30 +5,30 @@ import java.util.ArrayList;
 
 public class EmployeeTester {
     public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		try (Scanner input = new Scanner(System.in)) {
+			Employee newEmployee = new Employee("Sally","123 Sesame St","555-123-4567",10000);
+			System.out.println(newEmployee.toString());
+			System.out.print("Enter a new name: ");
+			String name = input.nextLine();
+			newEmployee.setName(name);
+			System.out.println(newEmployee.toString());
 
-		Employee newEmployee = new Employee("Sally","123 Sesame St","555-123-4567",10000);
-		System.out.println(newEmployee.toString());
-		System.out.print("Enter a new name: ");
-		String name = input.nextLine();
-		newEmployee.setName(name);
-		System.out.println(newEmployee.toString());
+			System.out.println(newEmployee.toString());
+			System.out.print("Enter a new position: ");
+			String position = input.nextLine();
+			newEmployee.setPosition(position);
+			System.out.println(newEmployee.toString());
 
-        System.out.println(newEmployee.toString());
-		System.out.print("Enter a new position: ");
-		String position = input.nextLine();
-		newEmployee.setPosition(position);
-		System.out.println(newEmployee.toString());
-
-        System.out.println(newEmployee.toString());
-		System.out.print("Enter a new salary: ");
-		int salary = input.nextInt();
-		newEmployee.setSalary(salary);
-		System.out.println(newEmployee.toString());
+			System.out.println(newEmployee.toString());
+			System.out.print("Enter a new salary: ");
+			int salary = input.nextInt();
+			newEmployee.setSalary(salary);
+			System.out.println(newEmployee.toString());
 
 
-		ArrayList<Employee> myEmployee = new ArrayList<Employee>();
-        myEmployee.add(newEmployee);
+			ArrayList<Employee> myEmployee = new ArrayList<Employee>();
+			myEmployee.add(newEmployee);
+		}
 		
 
 	} 
