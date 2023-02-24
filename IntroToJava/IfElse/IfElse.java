c2aba4b972585c0cd97237460e5b2ce0adc0e78f
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-02-23 23:04:30 -0600

    Update IFELSE

--- IntroToJava/IfElse/IfElse.java
+++ IntroToJava/IfElse/IfElse.java
@@ -3,23 +3,42 @@ import java.util.Scanner;
 
 public class IfElse {
     
+    // If Else
     public static void main(String [] args){
 
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        // Ask the user for their age
+        try (Scanner sc = new Scanner(System.in)) {
 
-        if (s.equals("tim")){
-            System.out.println("You typed tim");
+            // Ask the user for their age
+            String s = sc.nextLine();
+
+            // If the user inputted tim
+            if (s.equals("tim")){
+
+                // Print "You typed tim"
+                System.out.println("You typed tim");
+            }
+            
+            // else if the user typed hello
+            else if(s.equals("hello")){
+
+                // Print "Hi!"
+                System.out.println("Hi!");
+            }
+
+            // else if the user typed hi
+            else if(s.equals("hi")){
+
+                // Print "Hello!"
+                System.out.println("Hello!");
+            }
+
+            // else
+            else{
+
+                // Print "You typed __" 
+                System.out.println("You typed " + s);
+            }
         }
-        else if(s.equals("hello")){
-            System.out.println("Hi!");
-        }
-        else if(s.equals("hi")){
-            System.out.println("Hello!");
-        }
-        else{
-            System.out.println("You typed " + s);
-        }
-        
     }
 }
