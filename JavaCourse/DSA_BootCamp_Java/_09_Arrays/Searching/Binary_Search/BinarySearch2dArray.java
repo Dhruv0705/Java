09086c572c03e9c2f42bcf66fd21c19f37bfbcb0
Dhruv0705 <dhruvpatel329@yahoo.com>
2023-04-29 22:56:52 -0500

    Update Binary Search

--- JavaCourse/DSA_BootCamp_Java/_09_Arrays/Searching/Binary_Search/BinarySearch2dArray.java
+++ JavaCourse/DSA_BootCamp_Java/_09_Arrays/Searching/Binary_Search/BinarySearch2dArray.java
@@ -60,6 +60,8 @@ public class BinarySearch2dArray{
 
     /*
      * Q2: Search in a sorted matrix
+     * Leetcode: https://leetcode.com/problems/search-a-2d-matrix/
+     * 
      *     0  1  2  3  COLUMNS
      *  0 [1, 2, 3, 4]
      *  1 [5, 6, 7, 8]
@@ -77,8 +79,87 @@ public class BinarySearch2dArray{
      *     check weather the mid is 
     */
 
-    public SortedMatrixSearch(int[][] matrix, int target){
+    public static int [] SortedMatrixSearch(int[][] matrix, int target){
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        if (row == 1){
+            return binarySearch(matrix, target, 0, 0, col - 1);
+        }
+
+        if (col == 0){
+            return new int[]{-1, -1};
+        }
+
+        int rowStart = 0;
+        int rowEnd = row - 1;
+        int colMid = col / 2;
+
+        // run the loop till 2 rows are remaining
+        while(rowStart < (rowEnd - 1)) { // while this is true it will have more then two rows
+            int mid = rowStart + (rowEnd - rowStart) / 2;
+
+            // if the target is equal to the mid element, return the index
+            if (matrix[mid][colMid] == target) {
+                return new int[] {mid, colMid};
+            }
+
+            // if the target is less then the mid element, ignore the right half
+            if (matrix[mid][colMid] < target) {
+                rowStart = mid;
+            }
+
+            // if the target is greater then the mid element, ignore the left half
+            else {
+                rowEnd = mid;
+            }
+        }
+
+        // after the while loop is done, we will have two rows left
+        // check whether the target is present in the col of 2 rows
+
+        // now we have two rows
+        // check whether the target is in the col of 2 rows
+        if (matrix[rowStart][colMid] == target) {
+            return new int[]{rowStart, colMid};
+        }
+        if (matrix[rowStart + 1][colMid] == target) {
+            return new int[]{rowStart + 1, colMid};
+        }
+
+        // search in 1st half
+        if (target <= matrix[rowStart][colMid - 1]) {
+            return binarySearch(matrix, target, rowStart, 0, colMid-1);
+        }
+        // search in 2nd half
+        if (target >= matrix[rowStart][colMid + 1] && target <= matrix[rowStart][col - 1]) {
+            return binarySearch(matrix, target, rowStart, colMid + 1, col - 1);
+        }
+        // search in 3rd half
+        if (target <= matrix[rowStart + 1][colMid - 1]) {
+            return binarySearch(matrix, target, rowStart + 1, 0, colMid-1);
+        } else {
+            return binarySearch(matrix, target, rowStart + 1, colMid + 1, col - 1);
+        }
         
+    }
+
+    // search in the row provided between the colStart and colEnd indexes
+    static int[] binarySearch (int [][] matrix, int target, int row, int colStart, int colEnd){
+        while (colStart <= colEnd) {
+            int mid = colStart + (colEnd - colStart) / 2;
+
+            if (matrix[row][mid] == target) {
+                return new int []{row, mid};
+            }
+            if (matrix[row][mid] < target) {
+                colStart = mid + 1;
+            }
+            else {
+                colEnd = mid - 1;
+            }
+        }
+        return new int [] {-1, -1} ;
     }
 
     public static void main(String[] args) {
@@ -90,8 +171,14 @@ public class BinarySearch2dArray{
             {33, 34, 38, 50}
         };
 
-        int target = 37;
-        System.out.println(Arrays.toString(searchRowColumnMatrix(arr, target)));
-    
+        int [][] arr2 = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9 ,10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        System.out.println(Arrays.toString(searchRowColumnMatrix(arr, 29)));
+        System.out.println(Arrays.toString(SortedMatrixSearch(arr2, 16)));
     }
 }
